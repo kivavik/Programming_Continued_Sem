b0bc020f98e9d3145b542e0726902f759275e8ed
kivavik <dpkivva@mail.ru>
2023-10-21 09:22:07 +0300

    added 4 tasks for homework #1

---
+++ Sem1/Program.cs
@@ -0,0 +1,43 @@
+﻿// int firstNumber = 5;
+// int secondNumber = 6;
+// if (firstNumber > secondNumber)
+// {
+//     Console.WriteLine($"Первое число '{firstNumber}' больше второго числа '{secondNumber}'");
+// }
+// else if (firstNumber < secondNumber)
+// {
+//     Console.WriteLine($"Первое число '{firstNumber}' меньше второго числа '{secondNumber}'");
+// }
+// else
+// {
+//     Console.WriteLine($"Введенные числа равны '{firstNumber}'");
+// }
+
+// int a = 5, b = 7, c = 4;
+// int max = a;
+// if (a > b) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// int result = max;
+// Console.WriteLine(result);
+
+// int number = 6;
+// if (number % 2 > 0)
+// {
+//     Console.WriteLine($"Число: '{ number}' чётное");
+//       }
+// else
+// {
+//     Console.WriteLine($"Число: '{ number}' нечётное");
+//       }
+
+// int number = 7;
+// int count = 1;
+// while (count <= number)
+// {
+//     if (count % 2 == 0)
+//     {
+//         Console.Write($"{count}\t");
+//     }
+//     count++;
+// }
